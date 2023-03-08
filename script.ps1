124c370a9bb80733ad4f2adca77357ce89a3956b
Aaron Correya <aaroncorreya@microsoft.com>
2023-03-08 12:16:27 -0800

    Added generated files

---
+++ script.ps1
@@ -0,0 +1,8 @@
+﻿$content = Get-Content -Path .\template.json -raw
+New-Item -ItemType Directory ".\GeneratedRules"
+
+for (($i = 0); $i -lt 1000; $i++)
+{ 
+    $newGuid = [System.Guid]::NewGuid()
+    $content.replace("{guid}", $newGuid) | Out-File ".\GeneratedRules\$($newGuid).json" -Encoding utf8
+}
